4c232b91047f0e01c4b790637db3cce48a735aa9
Volodymyr Sych <volodymyr_sych@epam.com>
2025-03-02 11:48:31 +0200

    components-memory-seven with task

--- TSwithReact-basics/components-memory-seven/src/components/RequestTracker.tsx
+++ TSwithReact-basics/components-memory-seven/src/components/RequestTracker.tsx
@@ -2,6 +2,29 @@ import React, { useState } from 'react';
 
 import { delay } from '../utils/delay';
 
+// The Task
+// function RequestTracker() {
+//   const [pending, setPending] = useState(0);
+//   const [completed, setCompleted] = useState(0);
+
+//   async function handleClick() {
+//     setPending(p => p + 1);
+//     await delay(3000);
+//     setPending(pending - 1);
+//     setCompleted(c => c + 1);
+//   }
+
+//   return (
+//     <>
+//       <h3>
+//         Pending: {pending}
+//       </h3>
+//       <h3>
+//         Completed: {completed}
+//       </h3>
+//   }
+// }
+
 function RequestTracker(): React.ReactElement {
 	const [pending, setPending] = useState<number>(0);
 	const [completed, setCompleted] = useState<number>(0);
